b33408cc709da2952c558cdc53817fbc1d35a329
Oleksiy Khmelyevskyy <alex@92278df1-139a-4212-b609-0b8ac35fc34f.fritz.box>
2025-01-28 14:07:26 +0100

    Generated spring project

---
+++ src/main/java/com/alex/kitchensinkspringboot/KitchensinkSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.alex.kitchensinkspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchensinkSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KitchensinkSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alex/kitchensinkspringboot/KitchensinkSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.kitchensinkspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchensinkSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
